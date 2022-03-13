2a4b04391e3bb3ee24615fe2171bbf0e13197917
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-03-13 23:51:16 +0600

    display block and none add

---
+++ app.js
@@ -0,0 +1,31 @@
+const modifyBtn = () => {
+    document.getElementById('carList').innerText = 'Modify Car list'
+
+    document.getElementById('rendoemSection').style.display = 'none';
+    document.getElementById('sportsSection').style.display = 'none';
+    document.getElementById('jeepSection').style.display = 'none';
+
+    document.getElementById('modifySection').style.display = 'block';
+
+
+}
+
+const sportBtn = () => {
+    document.getElementById('carList').innerText = 'Sport Car list'
+
+    document.getElementById('rendoemSection').style.display = 'none';
+    document.getElementById('modifySection').style.display = 'none';
+    document.getElementById('jeepSection').style.display = 'none';
+
+    document.getElementById('sportsSection').style.display = 'block';
+}
+
+const jeepBtn = () => {
+    document.getElementById('carList').innerText = 'Jeep Car list'
+
+    document.getElementById('rendoemSection').style.display = 'none';
+    document.getElementById('modifySection').style.display = 'none';
+    document.getElementById('sportsSection').style.display = 'none';
+
+    document.getElementById('jeepSection').style.display = 'block';
+}
